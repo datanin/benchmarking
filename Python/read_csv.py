0cce54acdc5b0d97e00f83aa987a302312e384e4
datanin <jan.dombrowicz@outlook.com>
2018-01-10 11:52:11 +0100

    initial commit

---
+++ Python/read_csv.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import time
+import os
+
+os.getcwd()
+os.chdir("C:/Users/jando/OneDrive/dev/Projekte/forecast-grocery-sales")
+
+start_time = time.clock()
+train = pd.read_csv("data/train.csv")
+end_time = time.clock()
+
+round((end_time - start_time), 2)
+
+train.head()
+train.describe()
